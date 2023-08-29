0081e9fca889bc3dbbc1c8f39fdef3006c1cbbd0
Devendra Asus <devendrap@shriffle.com>
2023-08-29 07:57:43 +0530

    duplicate product variant not add

--- app/models/product_variant.rb
+++ app/models/product_variant.rb
@@ -1,5 +1,7 @@
 class ProductVariant < ApplicationRecord
   belongs_to :product
 
+  validates :product_id, uniqueness: { scope: [:width, :length], message: "Product has already been taken with this width and length"}
+
   validates :length, :width, presence: true, numericality: { greater_than: 0 }
 end
